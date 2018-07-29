868268c70e12fa240497926ff8d185fecc437125
Marcin Majkowski <pl.marcin.m@gmail.com>
2018-07-29 16:41:14 +0200

    Indent with 2 spaces

--- src/of-action.spec.ts
+++ src/of-action.spec.ts
@@ -4,78 +4,78 @@ import { ofAction } from './of-action';
 import { finalize } from 'rxjs/operators';
 
 class NoPayloadAction implements Action {
-    readonly type = 'no payload action';
+  readonly type = 'no payload action';
 }
 
 class FooAction implements Action {
-    readonly type = 'foo action';
+  readonly type = 'foo action';
 
-    constructor(public payload: string) {
-    }
+  constructor(public payload: string) {
+  }
 }
 
 class BarAction implements Action {
-    readonly type = 'bar action';
+  readonly type = 'bar action';
 
-    constructor(public payload: string) {
-    }
+  constructor(public payload: string) {
+  }
 }
 
 describe('ofAction', () => {
 
-    let spy: jasmine.Spy;
+  let spy: jasmine.Spy;
 
-    beforeEach(() => {
-        spy = jasmine.createSpy('spy');
-    });
+  beforeEach(() => {
+    spy = jasmine.createSpy('spy');
+  });
 
-    it('should pass action', done => {
-        const action = new NoPayloadAction();
-        from([action]).pipe(
-            ofAction(NoPayloadAction),
-            finalize(() => {
-                expect(spy).toHaveBeenCalledWith(action);
-                done();
-            })
-        ).subscribe(spy);
-    });
+  it('should pass action', done => {
+    const action = new NoPayloadAction();
+    from([action]).pipe(
+      ofAction(NoPayloadAction),
+      finalize(() => {
+        expect(spy).toHaveBeenCalledWith(action);
+        done();
+      })
+    ).subscribe(spy);
+  });
 
-    it('should block action', done => {
-        const action = new NoPayloadAction();
-        from([action]).pipe(
-            ofAction(FooAction),
-            finalize(() => {
-                expect(spy).not.toHaveBeenCalled();
-                done();
-            })
-        ).subscribe(spy);
-    });
+  it('should block action', done => {
+    const action = new NoPayloadAction();
+    from([action]).pipe(
+      ofAction(FooAction),
+      finalize(() => {
+        expect(spy).not.toHaveBeenCalled();
+        done();
+      })
+    ).subscribe(spy);
+  });
 
-    it('should pass actions', done => {
-        const action1 = new NoPayloadAction();
-        const action2 = new FooAction('foo');
-        const action3 = new BarAction('bar');
-        from([action1, action2, action3]).pipe(
-            ofAction(NoPayloadAction, FooAction),
-            finalize(() => {
-                expect(spy).toHaveBeenCalledWith(action1);
-                expect(spy).toHaveBeenCalledWith(action2);
-                expect(spy).not.toHaveBeenCalledWith(action3);
-                done();
-            })
-        ).subscribe(spy);
-    });
+  it('should pass actions', done => {
+    const action1 = new NoPayloadAction();
+    const action2 = new FooAction('foo');
+    const action3 = new BarAction('bar');
+    from([action1, action2, action3]).pipe(
+      ofAction(NoPayloadAction, FooAction),
+      finalize(() => {
+        expect(spy).toHaveBeenCalledWith(action1);
+        expect(spy).toHaveBeenCalledWith(action2);
+        expect(spy).not.toHaveBeenCalledWith(action3);
+        done();
+      })
+    ).subscribe(spy);
+  });
 
-    it('should allow to access payload', done => {
-        const action1 = new FooAction('foo');
-        const action2 = new NoPayloadAction();
-        let payload: string;
-        from([action1, action2]).pipe(
-            ofAction(FooAction, BarAction),
-            finalize(() => {
-                expect(payload).toBe('foo');
-                done();
-            })
-        ).subscribe(action => payload = action.payload);
-    });
+  it('should allow to access payload', done => {
+    const action1 = new FooAction('foo');
+    const action2 = new NoPayloadAction();
+    let payload: string;
+    from([action1, action2]).pipe(
+      ofAction(FooAction, BarAction),
+      finalize(() => {
+        expect(payload).toBe('foo');
+        done();
+      })
+    ).subscribe(action => payload = action.payload);
+  });
 });
--- src/of-action.ts
+++ src/of-action.ts
@@ -3,7 +3,7 @@ import { OperatorFunction } from 'rxjs/interfaces';
 import { filter } from 'rxjs/operators';
 
 export interface ActionClass<T extends Action> {
-    new (...args: any[]): T;
+  new(...args: any[]): T;
 }
 
 export function ofAction<R1 extends Action>(r1: ActionClass<R1>): OperatorFunction<Action, R1>;
@@ -13,8 +13,8 @@ export function ofAction<R1 extends Action, R2 extends Action, R3 extends Action
 export function ofAction<R1 extends Action, R2 extends Action, R3 extends Action, R4 extends Action, R5 extends Action>(r1: ActionClass<R1>, r2: ActionClass<R2>, r3: ActionClass<R3>, r4: ActionClass<R4>, r5: ActionClass<R5>): OperatorFunction<Action, R1 | R2 | R3 | R4 | R5>;
 export function ofAction<R1 extends Action, R2 extends Action, R3 extends Action, R4 extends Action, R5 extends Action, R6 extends Action>(r1: ActionClass<R1>, r2: ActionClass<R2>, r3: ActionClass<R3>, r4: ActionClass<R4>, r5: ActionClass<R5>, r6: ActionClass<R6>): OperatorFunction<Action, R1 | R2 | R3 | R4 | R5 | R6>;
 export function ofAction<R extends Action>(...allowedClasses: ActionClass<any>[]): OperatorFunction<Action, R> {
-    return filter(
-        (action: Action): action is R =>
-            allowedClasses.some(allowedClass => action instanceof allowedClass)
-    );
+  return filter(
+    (action: Action): action is R =>
+      allowedClasses.some(allowedClass => action instanceof allowedClass)
+  );
 }
